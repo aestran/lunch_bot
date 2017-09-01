e5567c3b08f6ecdccbd35f328b7c3804924aa130
Jamie Bentham <jbentham@.deloitte.co.uk>
2017-09-01 15:20:08 +0100

    Added mexican images for demo

--- services/zomato/Restaurant.js
+++ services/zomato/Restaurant.js
@@ -1,11 +1,12 @@
 var Restaurant = function (data) {
 this.name = data.name;
+this.id = data.R.res_id;
 this.address = data.location.address;
 this.city = data.location.city;
 this.lat = data.location.latitude;
 this.long = data.location.longitude;
 this.priceRange = data.price_range;
-this.image = data.thumb;
+this.images = data.photos_url;
 this.rating = data.user_rating.aggregate_rating;
 this.ratings = data.user_rating.votes;
 this.mainImage = data.featured_image;
--- services/zomatoService.js
+++ services/zomatoService.js
@@ -1,8 +1,7 @@
 var zomato = require('zomato');
 var Restaurant = require('./zomato/Restaurant.js');
 
-
-let selectedCuisines = ["British", "Chinese"];
+let selectedCuisines = ["Mexican"];
 
 let client = zomato.createClient({
     userKey: '4a3b30ef356a829e79e73f8886f0aa0e',
@@ -22,21 +21,32 @@ function restaurants(locations, cuisines) {
         entity_id: locations[0].id, //location id 
         entity_type: "city", // location type (city,subzone,zone , landmark, metro,group) 
         count: "3", // number of maximum result to display 
-        start: "1", //fetch results after offset 
+        start: "0", //fetch results after offset 
         radius: "10000", //radius around (lat,lon); to define search area, defined in meters(M) 
         cuisines: cuisineIds, //list of cuisine id's separated by comma 
-        sort: "rating", //choose any one out of these available choices 
-        order: "asc" // used with 'sort' parameter to define ascending(asc )/ descending(desc) 
+        sort: "best", //choose any one out of these available choices 
+        order: "desc" // used with 'sort' parameter to define ascending(asc )/ descending(desc) 
 
     }, function(err, restaurants) {
         if (!err) {
             let parsedResponse = JSON.parse(restaurants);
             let sortedRestaurents = new Array();
             parsedResponse.restaurants.forEach(function(restaurantData) {
+                console.log("Restaurant data " + JSON.stringify(restaurantData));
                 var restaurant = new Restaurant(restaurantData.restaurant);
                 sortedRestaurents.push(restaurant);
             })
             console.log("Results: " + JSON.stringify(sortedRestaurents));
+
+            sortedRestaurents.forEach(function(restaurant) {
+                if (restaurant.id == "16675470") {
+                    restaurant.mainImage = "https://b.zmtcdn.com/data/reviews_photos/4f6/e4938dc3f87c0bb187262df9d5e954f6_1482779714.jpg?fit=around%7C747%3A1328&crop=747%3A1328%3B%2A%2C%2A";
+                } else if (restaurant.id == "16675084") {
+                    restaurant.mainImage = "https://b.zmtcdn.com/data/reviews_photos/4f6/e4938dc3f87c0bb187262df9d5e954f6_1482779714.jpg?fit=around%7C747%3A1328&crop=747%3A1328%3B%2A%2C%2A";
+                } else if (restaurant.id == "16674933") {
+
+                }
+            })
 
             return sortedRestaurents;
         } else {
